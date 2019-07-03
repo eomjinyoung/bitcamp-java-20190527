93a23a1f638d38e37a02713569b95d98b18b3c34
eomjinyoung <jinyoung.eom@gmail.com>
2019-07-03 16:46:17 +0900

    bitcamp-java-application 01 버전 완료

---
+++ bitcamp-java-application/src/main/java/com/eomcs/lms/App.java
@@ -0,0 +1,13 @@
+// 애플리케이션 메인 클래스
+// => 애플리케이션을 실행할 때 이 클래스를 실행한다.
+package com.eomcs.lms;
+
+public class App {
+    public String getGreeting() {
+        return "Hello world.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
