e637ccc42c65fcb3bf1470853f56b8fbe2ce7d1e
eomjinyoung <jinyoung.eom@gmail.com>
2019-07-05 12:11:13 +0900

    bitcamp-java-application v05

--- bitcamp-java-application/src/main/java/com/eomcs/lms/App.java
+++ bitcamp-java-application/src/main/java/com/eomcs/lms/App.java
@@ -5,39 +5,67 @@ package com.eomcs.lms;
 import java.util.Scanner;
 
 public class App {
+  
+  static Scanner keyScan;
+  
   public static void main(String[] args) {
     java.io.InputStream keyboard = System.in;
-    Scanner keyScan = new Scanner(keyboard);
+    keyScan = new Scanner(keyboard);
     
-    System.out.print("번호? ");
-    String no = keyScan.nextLine();
     
-    System.out.print("수업명? ");
-    String lectureName = keyScan.nextLine();
-    
-    System.out.print("설명? ");
-    String description = keyScan.nextLine();
-    
-    System.out.print("시작일? ");
-    String startDate = keyScan.nextLine();
-    
-    System.out.print("종료일? ");
-    String endDate = keyScan.nextLine();
-    
-    System.out.print("총수업시간? ");
-    String totalHours = keyScan.nextLine();
-    
-    System.out.print("일수업시간? ");
-    String dayHours = keyScan.nextLine();
-    
+    int no = getIntValue("번호? ");
+    String lectureName = getStringValue("수업명? ");
+    String description = getStringValue("설명? ");
+    java.sql.Date startDate = getDateValue("시작일? ");
+    java.sql.Date endDate = getDateValue("종료일? ");
+    int totalHours = getIntValue("총수업시간? ");
+    int dayHours = getIntValue("일수업시간? ");
+
     System.out.println();
     
-    System.out.println("번호: " + no);
-    System.out.println("수업명: " + lectureName);
-    System.out.println("설명: " + description);
-    System.out.println("시작일: " + startDate);
-    System.out.println("종료일: " + endDate);
-    System.out.println("총수업시간: " + totalHours);
-    System.out.println("일수업시간: " + dayHours);
+    System.out.printf("번호: %s\n", no);
+    System.out.printf("수업명: %s\n", lectureName);
+    System.out.printf("설명: %s\n", description);
+    System.out.printf("시작일: %s\n", startDate);
+    System.out.printf("종료일: %s\n", endDate);
+    System.out.printf("총수업시간: %s\n", totalHours);
+    System.out.printf("일수업시간: %s\n", dayHours);
+  }
+  
+  private static int getIntValue(String message) {
+    while (true) {
+      try {
+        System.out.print(message);
+        return Integer.parseInt(keyScan.nextLine());
+      } catch (NumberFormatException e) {
+        System.out.println("숫자를 입력하세요.");
+      }
+    }
+  }
+  
+  private static java.sql.Date getDateValue(String message) {
+    while (true) {
+      try {
+        System.out.print(message);
+        return java.sql.Date.valueOf(keyScan.nextLine());
+      } catch (IllegalArgumentException e) {
+        System.out.println("2019-07-05 형식으로 입력하세요.");
+      }
+    }
+  }
+  
+  private static String getStringValue(String message) {
+    System.out.print(message);
+    return keyScan.nextLine();
   }
 }
+
+
+
+
+
+
+
+
+
+
--- bitcamp-java-application/src/main/java/com/eomcs/lms/App3.java
+++ bitcamp-java-application/src/main/java/com/eomcs/lms/App3.java
@@ -1,10 +1,31 @@
 package com.eomcs.lms;
 
+import java.sql.Date;
+import java.util.Scanner;
+
 public class App3 {
+
   public static void main(String[] args) {
-    System.out.println("번호: 1");
-    System.out.println("내용: 게시글입니다.");
-    System.out.println("작성일: 2019-01-01");
-    System.out.println("조회수: 0");
+    Scanner keyboard = new Scanner(System.in);
+
+    System.out.print("번호? ");
+    int no = Integer.parseInt(keyboard.nextLine());
+    
+    System.out.print("내용? ");
+    String contents = keyboard.nextLine();
+    
+    Date createdDate = new Date(System.currentTimeMillis()); 
+    
+    int viewCount = 0;
+    
+    // 사용후 스캐너 객체의 자원을 해제한다.
+    keyboard.close();
+    
+    System.out.println(); // 빈 줄 출력
+    
+    System.out.printf("번호: %d\n", no);
+    System.out.printf("내용: %s\n", contents);
+    System.out.printf("작성일: %s\n", createdDate);
+    System.out.printf("조회수: %d\n", viewCount);
   }
 }
