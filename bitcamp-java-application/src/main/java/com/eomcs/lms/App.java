d788405adcd787022e667828f66c0531dafe1e36
eomjinyoung <jinyoung.eom@gmail.com>
2019-07-05 18:02:02 +0900

    bitcamp-java-application 07 버전

--- bitcamp-java-application/src/main/java/com/eomcs/lms/App.java
+++ bitcamp-java-application/src/main/java/com/eomcs/lms/App.java
@@ -13,24 +13,25 @@ public class App {
     java.io.InputStream keyboard = System.in;
     keyScan = new Scanner(keyboard);
     
-    int[] no = new int[100];
-    String[] lectureName = new String[100];
-    String[] description = new String[100];
-    Date[] startDate = new Date[100];
-    Date[] endDate = new Date[100];
-    int[] totalHours = new int[100];
-    int[] dayHours = new int[100];
-    
+    Lesson[] lessons = new Lesson[100];
     
     int i = 0;
-    for ( ; i < no.length; i++) {
-      no[i] = getIntValue("번호? ");
-      lectureName[i] = getStringValue("수업명? ");
-      description[i] = getStringValue("설명? ");
-      startDate[i] = getDateValue("시작일? ");
-      endDate[i] = getDateValue("종료일? ");
-      totalHours[i] = getIntValue("총수업시간? ");
-      dayHours[i] = getIntValue("일수업시간? ");
+    for ( ; i < lessons.length; i++) {
+      
+      // 수업 데이터를 저장할 메모리를 Lesson 설계도에 따라 만든다.
+      Lesson lesson = new Lesson();
+      
+      // 사용자가 입력한 값을 Lesson 인스턴스의 각 변수에 저장한다.
+      lesson.no = getIntValue("번호? ");
+      lesson.title = getStringValue("수업명? ");
+      lesson.contents = getStringValue("설명? ");
+      lesson.startDate = getDateValue("시작일? ");
+      lesson.endDate = getDateValue("종료일? ");
+      lesson.totalHours = getIntValue("총수업시간? ");
+      lesson.dayHours = getIntValue("일수업시간? ");
+      
+      // 수업 데이터를 저장하고 있는 인스턴스의 주소를 레퍼런스 배열에 저장한다.
+      lessons[i] = lesson;
       
       System.out.print("계속 입력하시겠습니까?(Y/n) ");
       String response = keyScan.nextLine();
@@ -42,8 +43,14 @@ public class App {
     System.out.println();
     
     for (int i2 = 0; i2 <= i; i2++) {
+      
+      // 레퍼런스 배열에서 한 개의 인스턴스 주소를 꺼낸다.
+      Lesson lesson = lessons[i2];
+      
+      // 그 인스턴스 주소로 찾아가서 인스턴스의 각 변수 값을 꺼내 출력한다.
       System.out.printf("%s, %s, %s ~ %s, %s\n", 
-          no[i2], lectureName[i2], startDate[i2], endDate[i2], totalHours[i2]);
+          lesson.no, lesson.title, 
+          lesson.startDate, lesson.endDate, lesson.totalHours);
     }
   }
   
