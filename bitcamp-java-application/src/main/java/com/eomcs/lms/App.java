aae43bb7e12c4ded7ea86bcda9ed4eb10b29a2c8
eomjinyoung <jinyoung.eom@gmail.com>
2019-07-08 11:07:05 +0900

    bitcamp-java-application 08 버전

--- bitcamp-java-application/src/main/java/com/eomcs/lms/App.java
+++ bitcamp-java-application/src/main/java/com/eomcs/lms/App.java
@@ -18,39 +18,36 @@ public class App {
     int i = 0;
     for ( ; i < lessons.length; i++) {
       
-      // 수업 데이터를 저장할 메모리를 Lesson 설계도에 따라 만든다.
-      Lesson lesson = new Lesson();
+      System.out.print("명령> ");
+      String command = keyScan.nextLine();
       
-      // 사용자가 입력한 값을 Lesson 인스턴스의 각 변수에 저장한다.
-      lesson.no = getIntValue("번호? ");
-      lesson.title = getStringValue("수업명? ");
-      lesson.contents = getStringValue("설명? ");
-      lesson.startDate = getDateValue("시작일? ");
-      lesson.endDate = getDateValue("종료일? ");
-      lesson.totalHours = getIntValue("총수업시간? ");
-      lesson.dayHours = getIntValue("일수업시간? ");
-      
-      // 수업 데이터를 저장하고 있는 인스턴스의 주소를 레퍼런스 배열에 저장한다.
-      lessons[i] = lesson;
-      
-      System.out.print("계속 입력하시겠습니까?(Y/n) ");
-      String response = keyScan.nextLine();
-      
-      if (response.equals("n"))
-        break;
-    }
-    
-    System.out.println();
-    
-    for (int i2 = 0; i2 <= i; i2++) {
-      
-      // 레퍼런스 배열에서 한 개의 인스턴스 주소를 꺼낸다.
-      Lesson lesson = lessons[i2];
-      
-      // 그 인스턴스 주소로 찾아가서 인스턴스의 각 변수 값을 꺼내 출력한다.
-      System.out.printf("%s, %s, %s ~ %s, %s\n", 
-          lesson.no, lesson.title, 
-          lesson.startDate, lesson.endDate, lesson.totalHours);
+      if (command.equals("/lesson/add")) {
+        // 수업 데이터를 저장할 메모리를 Lesson 설계도에 따라 만든다.
+        Lesson lesson = new Lesson();
+        
+        // 사용자가 입력한 값을 Lesson 인스턴스의 각 변수에 저장한다.
+        lesson.no = getIntValue("번호? ");
+        lesson.title = getStringValue("수업명? ");
+        lesson.contents = getStringValue("설명? ");
+        lesson.startDate = getDateValue("시작일? ");
+        lesson.endDate = getDateValue("종료일? ");
+        lesson.totalHours = getIntValue("총수업시간? ");
+        lesson.dayHours = getIntValue("일수업시간? ");
+        
+        // 수업 데이터를 저장하고 있는 인스턴스의 주소를 레퍼런스 배열에 저장한다.
+        lessons[i] = lesson;
+        
+      } else if (command.equals("/lesson/list")) {
+        for (int i2 = 0; i2 <= i; i2++) {
+          Lesson lesson = lessons[i2];
+          System.out.printf("%s, %s, %s ~ %s, %s\n", 
+              lesson.no, lesson.title, 
+              lesson.startDate, lesson.endDate, lesson.totalHours);
+        }
+        
+      } else {
+        System.out.println("해당 명령을 지원하지 않습니다!");
+      }
     }
   }
   
