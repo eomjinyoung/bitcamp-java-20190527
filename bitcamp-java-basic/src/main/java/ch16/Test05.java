f73547d6530601e5b0f468eebc3c0fd8c8963071
eomjinyoung <jinyoung.eom@gmail.com>
2019-07-17 16:54:06 +0900

    제네릭 예제 변경

--- bitcamp-java-basic/src/main/java/ch16/Test05.java
+++ bitcamp-java-basic/src/main/java/ch16/Test05.java
@@ -28,7 +28,8 @@ public class Test05 {
   
   // 예1) 제네릭의 타입 파라미터로 레퍼런스 배열을 생성할 수 없다.
   static <T> T[] create1() {
-    //T[] arr = new T[10]; // 컴파일 오류!
+    T[] arr;
+    //arr = new T[10]; // 컴파일 오류! new 명령어를 사용할 때 제네릭의 타입 파라미터를 사용할 수 없다.
     return null;
   }
   
@@ -48,8 +49,6 @@ public class Test05 {
   // 예4) 견본 배열의 타입 정보를 가지고 배열을 생성하기
   @SuppressWarnings("unchecked")
   static <T> T[] create4(T[] arr) {
-    // copyOf(original, newLength)
-    // => 원래 배열(original)과 같은 타입의 배열을 배열크기(newLength)에 맞춰 새로 생성한다.
     return (T[]) Array.newInstance(arr.getClass(), 10);
   }
   
