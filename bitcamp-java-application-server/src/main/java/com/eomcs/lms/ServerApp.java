3dfce8838b49fbe3551d0f8f877b831268936477
eomjinyoung <jinyoung.eom@gmail.com>
2019-08-02 10:54:25 +0900

    bitcamp-java-application 32 버전

--- bitcamp-java-application-server/src/main/java/com/eomcs/lms/ServerApp.java
+++ bitcamp-java-application-server/src/main/java/com/eomcs/lms/ServerApp.java
@@ -46,6 +46,15 @@ public class ServerApp {
             case "/member/list":
               listMember();
               break;
+            case "/member/delete":
+              deleteMember();
+              break;  
+            case "/member/detail":
+              detailMember();
+              break;
+            case "/member/update":
+              updateMember();
+              break;
             case "quit":
               out.writeUTF("ok");
               break loop;
@@ -69,6 +78,101 @@ public class ServerApp {
     System.out.println("서버 종료!");
   }
   
+  private static void updateMember0() throws Exception {
+    Member member = (Member)in.readObject();
+    
+    for (Member m : memberList) {
+      if (m.getNo() == member.getNo()) {
+        // 클라이언트가 보낸 값으로 기존 객체의 값을 바꾼다.
+        m.setName(member.getName());
+        m.setEmail(member.getEmail());
+        m.setPassword(member.getPassword());
+        m.setPhoto(member.getPhoto());
+        m.setTel(member.getTel());
+        m.setRegisteredDate(member.getRegisteredDate());
+        
+        out.writeUTF("ok");
+        return;
+      }
+    }
+    
+    fail("해당 번호의 회원이 없습니다.");
+  }
+  
+  private static void updateMember() throws Exception {
+    Member member = (Member)in.readObject();
+    
+    int index = indexOfMember(member.getNo());
+    if (index == -1) {
+      fail("해당 번호의 회원이 없습니다.");
+      return;
+    }
+    memberList.set(index, member);
+    out.writeUTF("ok");
+    
+    /*
+    for (int i = 0; i < memberList.size(); i++) {
+      Member m = memberList.get(i);
+      if (m.getNo() == member.getNo()) {
+        // 기존 객체를 클라이언트가 보낸로 교체한다. 
+        memberList.set(i, member);
+        out.writeUTF("ok");
+        return;
+      }
+    }
+    out.writeUTF("fail");
+    out.writeUTF("해당 번호의 회원이 없습니다.");
+    */
+  }
+
+  private static void detailMember() throws Exception {
+    int no = in.readInt();
+    
+    int index = indexOfMember(no);
+    if (index == -1) {
+      fail("해당 번호의 회원이 없습니다.");
+      return;
+    }
+    out.writeUTF("ok");
+    out.writeObject(memberList.get(index));
+    
+    /*
+    for (Member m : memberList) {
+      if (m.getNo() == no) {
+        out.writeUTF("ok");
+        out.writeObject(m);
+        return;
+      }
+    }
+    out.writeUTF("fail");
+    out.writeUTF("해당 번호의 회원이 없습니다.");
+    */
+  }
+
+  private static void deleteMember() throws Exception {
+    int no = in.readInt();
+    
+    int index = indexOfMember(no);
+    if (index == -1) {
+      fail("해당 번호의 회원이 없습니다.");
+      return;
+    }
+    memberList.remove(index);
+    out.writeUTF("ok");
+    
+    /*
+    for (Member m : memberList) {
+      if (m.getNo() == no) {
+        memberList.remove(m);
+        out.writeUTF("ok");
+        return;
+      }
+    }
+    out.writeUTF("fail");
+    out.writeUTF("해당 번호의 회원이 없습니다.");
+    */
+  }
+
   private static void addMember() throws Exception {
     Member member = (Member)in.readObject();
     memberList.add(member);
@@ -77,7 +181,24 @@ public class ServerApp {
   
   private static void listMember() throws Exception {
     out.writeUTF("ok");
+    out.reset(); // 기존에 serialize 했던 객체의 상태를 무시하고 다시 serialize 한다.
     out.writeObject(memberList);
+  }
+  
+  private static int indexOfMember(int no) {
+    int i = 0;
+    for (Member m : memberList) {
+      if (m.getNo() == no) {
+        return i;
+      }
+      i++;
+    }
+    return -1;
+  }
+  
+  private static void fail(String cause) throws Exception {
+    out.writeUTF("fail");
+    out.writeUTF(cause);
   }
 }
 
--- bitcamp-java-application-server/src/main/java/com/eomcs/lms/ServerTest.java
+++ bitcamp-java-application-server/src/main/java/com/eomcs/lms/ServerTest.java
@@ -60,6 +60,33 @@ public class ServerTest {
       }
       System.out.println("------------------");
       
+      if (!list()) {
+        error();
+      }
+      System.out.println("------------------");
+      
+      if (!detail()) {
+        error();
+      }
+      System.out.println("------------------");
+      
+      member = new Member();
+      member.setNo(1);
+      member.setName("홍길동2");
+      member.setEmail("hong2@test.com");
+      member.setPhoto("hong.gif");
+      member.setTel("1111-1111");
+      
+      if (!update(member)) {
+        error();
+      }
+      System.out.println("------------------");
+      
+      if (!list()) {
+        error();
+      }
+      System.out.println("------------------");
+      
       if (!quit()) {
         error();
       }
@@ -76,6 +103,7 @@ public class ServerTest {
     System.out.println("서버와 연결 끊음.");
   }
   
+
   private static void error() throws Exception {
     System.out.printf("오류: %s\n", in.readUTF());
   }
@@ -94,8 +122,37 @@ public class ServerTest {
 
   private static boolean delete() throws Exception {
     out.writeUTF("/member/delete");
+    out.writeInt(2);
     out.flush();
     System.out.print("delete 요청함 => ");
+    
+    if (!in.readUTF().equals("ok"))
+      return false;
+    
+    System.out.println("처리 완료!");
+    return true;
+  }
+  
+  private static boolean detail() throws Exception {
+    out.writeUTF("/member/detail");
+    out.writeInt(1);
+    out.flush();
+    System.out.print("detail 요청함 => ");
+    
+    if (!in.readUTF().equals("ok"))
+      return false;
+    
+    System.out.println("처리 완료!");
+    System.out.println(in.readObject());
+    
+    return true;
+  }
+  
+  private static boolean update(Member m) throws Exception {
+    out.writeUTF("/member/update");
+    out.writeObject(m);
+    out.flush();
+    System.out.print("update 요청함 => ");
     
     if (!in.readUTF().equals("ok"))
       return false;
@@ -120,6 +177,7 @@ public class ServerTest {
     for (Member m : list) {
       System.out.println(m);
     }
+    list.clear();
     return true;
   }
 
