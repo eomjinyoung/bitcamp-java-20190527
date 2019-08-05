d9937c6ba4e594f85e857343a6850cbf9976f837
eomjinyoung <jinyoung.eom@gmail.com>
2019-08-05 10:52:05 +0900

    bitcamp-java-application 32_13 버전

--- bitcamp-java-application-server/src/main/java/com/eomcs/lms/dao/csv/AbstractCsvDataSerializer.java
+++ bitcamp-java-application-server/src/main/java/com/eomcs/lms/dao/csv/AbstractCsvDataSerializer.java
@@ -1,5 +1,6 @@
 package com.eomcs.lms.dao.csv;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -7,7 +8,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.ObjectOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 
 public abstract class AbstractCsvDataSerializer<T,K> {
@@ -22,26 +23,39 @@ public abstract class AbstractCsvDataSerializer<T,K> {
     this.file = new File(file);
   }
   
-  @SuppressWarnings("unchecked")
   protected void loadData() throws IOException, ClassNotFoundException {
+    
     try (BufferedReader in = new BufferedReader(
         new InputStreamReader(new FileInputStream(file)))) {
       
-      while ((String line = in.readLine()) != null) {
-        
+      String line = null;
+      while ((line = in.readLine()) != null) {
+        String[] values = line.split(",");
+        T obj = createObject(values);
+        list.add(obj);
       }
-      
-      //list = (ArrayList<T>) in.readObject();
     }
   }
   
   protected void saveData() throws FileNotFoundException, IOException {
-    try (
-      ObjectOutputStream out = new ObjectOutputStream(
-          new FileOutputStream(file))) {
-      out.writeObject(list);
+    try (PrintStream out = new PrintStream(
+        new BufferedOutputStream(new FileOutputStream(file)))) {
+      
+      for (T obj : list) {
+        out.println(createCSV(obj));
+      }
     }
   }
+  
+  // 서브 클래스에게 문자열 배열에 대해 객체를 생성하는 책임을 넘긴다.
+  // => 위의 loadData() 메서드에서 기본 기능을 정의하고, 
+  //    특정 기능에 대해서는  
+  //    서브 클래스에서 완성하도록 유도하는 설계 기법을 
+  //    "template method" 패턴이라 부른다.
+  //
+  protected abstract T createObject(String[] values);
+  protected abstract String createCSV(T obj);
+  
   
   // 서브 클래스에서 특정 값을 가지고 데이터를 찾는 기능을 반드시 구현하도록 강제하자!
   public abstract int indexOf(K key);
--- bitcamp-java-application-server/src/main/java/com/eomcs/lms/dao/csv/BoardCsvDao.java
+++ bitcamp-java-application-server/src/main/java/com/eomcs/lms/dao/csv/BoardCsvDao.java
@@ -2,19 +2,20 @@ package com.eomcs.lms.dao.csv;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.Date;
 import java.util.List;
 import com.eomcs.lms.domain.Board;
 
 public class BoardCsvDao extends AbstractCsvDataSerializer<Board,Integer> {
   
-  public BoardCsvDao(String file) throws ClassNotFoundException {
+  public BoardCsvDao(String file) {
     super(file);
     
     try {
       loadData();
       System.out.println("게시물 데이터 로딩 완료!");
       
-    } catch (IOException e) {
+    } catch (Exception e) {
       System.out.println("게시물 데이터 로딩 중 오류 발생!");
     }
   }
@@ -34,6 +35,31 @@ public class BoardCsvDao extends AbstractCsvDataSerializer<Board,Integer> {
     }
   }
   
+  // 수퍼 클래스에서 template method(loadSave())를 정의하고 
+  // 객체 생성의 구체적인 구현은 서브 클래스에서 완성해야 한다.
+  @Override
+  protected Board createObject(String[] values) {
+    // 파일에서 읽어 들인 데이터의 CSV 형식은 다음과 같다고 가정하자!
+    // => 번호,내용,생성일,조회수
+    //
+    Board board = new Board();
+    board.setNo(Integer.parseInt(values[0]));
+    board.setContents(values[1]);
+    board.setCreatedDate(Date.valueOf(values[2]));
+    board.setViewCount(Integer.parseInt(values[3]));
+    
+    return board;
+  }
+  
+  @Override
+  protected String createCSV(Board obj) {
+    return String.format("%d,%s,%s,%d",
+        obj.getNo(), 
+        obj.getContents(),
+        obj.getCreatedDate(),
+        obj.getViewCount());
+  }
+  
   @Override
   public int indexOf(Integer key) {
     int i = 0;
@@ -46,16 +72,16 @@ public class BoardCsvDao extends AbstractCsvDataSerializer<Board,Integer> {
     return -1;
   }
   
-  public int insert(Board board) throws Exception {
+  public int add(Board board) throws Exception {
     list.add(board);
     return 1;
   }
   
-  public List<Board> findAll() throws Exception {
+  public List<Board> list() throws Exception {
     return list;
   }
   
-  public Board findBy(int no) throws Exception {
+  public Board get(int no) throws Exception {
     int index = indexOf(no);
     if (index == -1)
       return null;
@@ -63,7 +89,7 @@ public class BoardCsvDao extends AbstractCsvDataSerializer<Board,Integer> {
     return list.get(index);
   }
   
-  public int update(Board board) throws Exception {
+  public int modify(Board board) throws Exception {
     int index = indexOf(board.getNo());
     if (index == -1)
       return 0;
@@ -72,7 +98,7 @@ public class BoardCsvDao extends AbstractCsvDataSerializer<Board,Integer> {
     return 1;
   }
   
-  public int delete(int no) throws Exception {
+  public int remove(int no) throws Exception {
     int index = indexOf(no);
     if (index == -1)
       return 0;
