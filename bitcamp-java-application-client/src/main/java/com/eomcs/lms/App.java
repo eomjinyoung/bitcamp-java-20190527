9e5998660a1beb84c77e9a2f6c3446790d500a09
eomjinyoung <jinyoung.eom@gmail.com>
2019-08-06 11:23:27 +0900

    bitcamp-java-application C/S 33 버전

--- bitcamp-java-application-client/src/main/java/com/eomcs/lms/App.java
+++ bitcamp-java-application-client/src/main/java/com/eomcs/lms/App.java
@@ -1,9 +1,6 @@
 // client-v33_2 : Stateful 통신 방식을 Stateless 통신 방식으로 변경한다. 
 package com.eomcs.lms;
 
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.HashMap;
@@ -42,86 +39,73 @@ public class App {
   Scanner keyScan;
 
   private void service() {
-    try (Socket socket = new Socket("localhost", 8888);
-        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-        ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
+    // Command 객체가 사용할 데이터 처리 객체를 준비한다.
+    BoardDao boardDao = new BoardDaoProxy("localhost", 8888);
+    MemberDao memberDao = new MemberDaoProxy("localhost", 8888);
+    LessonDao lessonDao = new LessonDaoProxy("localhost", 8888);
 
-      // Command 객체가 사용할 데이터 처리 객체를 준비한다.
-      BoardDao boardDao = new BoardDaoProxy(in, out);
-      MemberDao memberDao = new MemberDaoProxy(in, out);
-      LessonDao lessonDao = new LessonDaoProxy(in, out);
-      
-      keyScan = new Scanner(System.in);
+    keyScan = new Scanner(System.in);
 
-      Deque<String> commandStack = new ArrayDeque<>();
-      Queue<String> commandQueue = new LinkedList<>();
+    Deque<String> commandStack = new ArrayDeque<>();
+    Queue<String> commandQueue = new LinkedList<>();
 
-      Input input = new Input(keyScan);
+    Input input = new Input(keyScan);
 
-      HashMap<String,Command> commandMap = new HashMap<>();
+    HashMap<String,Command> commandMap = new HashMap<>();
 
-      commandMap.put("/lesson/add", new LessonAddCommand(input, lessonDao));
-      commandMap.put("/lesson/delete", new LessonDeleteCommand(input, lessonDao));
-      commandMap.put("/lesson/detail", new LessonDetailCommand(input, lessonDao));
-      commandMap.put("/lesson/list", new LessonListCommand(input, lessonDao));
-      commandMap.put("/lesson/update", new LessonUpdateCommand(input, lessonDao));
+    commandMap.put("/lesson/add", new LessonAddCommand(input, lessonDao));
+    commandMap.put("/lesson/delete", new LessonDeleteCommand(input, lessonDao));
+    commandMap.put("/lesson/detail", new LessonDetailCommand(input, lessonDao));
+    commandMap.put("/lesson/list", new LessonListCommand(input, lessonDao));
+    commandMap.put("/lesson/update", new LessonUpdateCommand(input, lessonDao));
 
-      commandMap.put("/member/add", new MemberAddCommand(input, memberDao));
-      commandMap.put("/member/delete", new MemberDeleteCommand(input, memberDao));
-      commandMap.put("/member/detail", new MemberDetailCommand(input, memberDao));
-      commandMap.put("/member/list", new MemberListCommand(input, memberDao));
-      commandMap.put("/member/update", new MemberUpdateCommand(input, memberDao));
+    commandMap.put("/member/add", new MemberAddCommand(input, memberDao));
+    commandMap.put("/member/delete", new MemberDeleteCommand(input, memberDao));
+    commandMap.put("/member/detail", new MemberDetailCommand(input, memberDao));
+    commandMap.put("/member/list", new MemberListCommand(input, memberDao));
+    commandMap.put("/member/update", new MemberUpdateCommand(input, memberDao));
 
-      commandMap.put("/board/add", new BoardAddCommand(input, boardDao));
-      commandMap.put("/board/delete", new BoardDeleteCommand(input, boardDao));
-      commandMap.put("/board/detail", new BoardDetailCommand(input, boardDao));
-      commandMap.put("/board/list", new BoardListCommand(input, boardDao));
-      commandMap.put("/board/update", new BoardUpdateCommand(input, boardDao));
+    commandMap.put("/board/add", new BoardAddCommand(input, boardDao));
+    commandMap.put("/board/delete", new BoardDeleteCommand(input, boardDao));
+    commandMap.put("/board/detail", new BoardDetailCommand(input, boardDao));
+    commandMap.put("/board/list", new BoardListCommand(input, boardDao));
+    commandMap.put("/board/update", new BoardUpdateCommand(input, boardDao));
 
-      commandMap.put("/hi", new HiCommand(input));
-      commandMap.put("/calc/plus", new CalcPlusCommand(input));
+    commandMap.put("/hi", new HiCommand(input));
+    commandMap.put("/calc/plus", new CalcPlusCommand(input));
 
-      while (true) {
+    while (true) {
 
-        String command = prompt();
+      String command = prompt();
 
-        if (command.length() == 0)
-          continue;
+      if (command.length() == 0)
+        continue;
 
-        commandStack.push(command); 
-        commandQueue.offer(command); 
+      commandStack.push(command); 
+      commandQueue.offer(command); 
 
-        Command executor = commandMap.get(command);
+      Command executor = commandMap.get(command);
 
-        if (command.equals("quit")) {
-          out.writeUTF(command);
-          out.flush();
-          break;
-          
-        } else if (command.equals("serverstop")) {
-          out.writeUTF(command);
-          out.flush();
-          break;
-          
-        } else if (command.equals("history")) {
-          printCommandHistory(commandStack);
+      if (command.equals("serverstop")) {
+        //out.writeUTF(command);
+        //out.flush();
+        break;
 
-        } else if (command.equals("history2")) {
-          printCommandHistory(commandQueue);
+      } else if (command.equals("history")) {
+        printCommandHistory(commandStack);
 
-        } else if (executor != null) {
-          executor.execute();
+      } else if (command.equals("history2")) {
+        printCommandHistory(commandQueue);
 
-        } else {
-          System.out.println("해당 명령을 지원하지 않습니다!");
-        }
+      } else if (executor != null) {
+        executor.execute();
 
-        System.out.println();
-      } //while
+      } else {
+        System.out.println("해당 명령을 지원하지 않습니다!");
+      }
 
-    } catch (Exception e) {
-      System.out.println("서버 통신 오류!");
-    }
+      System.out.println();
+    } //while
   }
 
   private void printCommandHistory(Iterable<String> list) {
@@ -141,7 +125,7 @@ public class App {
     System.out.print("명령> ");
     return keyScan.nextLine();
   }
-  
+
   public static void main(String[] args) {
     App app = new App();
     app.service();
--- bitcamp-java-application-server/src/main/java/com/eomcs/lms/client/MemberDaoProxy.java
+++ bitcamp-java-application-server/src/main/java/com/eomcs/lms/client/MemberDaoProxy.java
@@ -2,93 +2,110 @@ package com.eomcs.lms.client;
 
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.Socket;
 import java.util.List;
 import com.eomcs.lms.dao.MemberDao;
 import com.eomcs.lms.domain.Member;
 
-// 서버쪽의 MemberDao를 대행할 프록시 객체를 정의한다.
-// => 클라이언트는 이 프록시 객체를 통하여 
-//    서버쪽의 MemberDao 객체를 사용할 것이다. 
-// => 보통 서비스를 제공하는 서버쪽에서 프록시 객체를 만들어
-//    클라이언트 개발자에게 배포한다.
-// => 이런 방식으로 프로그램을 개발할 때 이점은,
-//    클라이언트 개발자가 서버와 어떻게 통신해야 하는지 알 필요가 없다는 것이다.
-// => 즉 서버쪽과 통신하는 코드를 캡슐화하여 감추고, 대신 메서드를 통해 단순화시키는 기법이다.
-//    이런 설계 기법을 "프록시 패턴(proxy pattern)"이라 한다.
+//Stateful 통신 방식을 Stateless 통신 방식으로 변경한다.
+//=> 매번 요청할 때마다 서버와 연결한다.
+//=> 서버의 응답을 받으면 연결을 끊는다.
 //
-// 프록시 패턴
-// => 프록시 역할을 수행할 클래스는 실제 일을 하는 클래스와 같은 규칙을 따라야 한다.
-// 
 public class MemberDaoProxy implements MemberDao {
 
-  ObjectInputStream in;
-  ObjectOutputStream out;
-  
-  public MemberDaoProxy(ObjectInputStream in, ObjectOutputStream out) {
-    this.in = in;
-    this.out = out;
+  String host;
+  int port;
+
+  public MemberDaoProxy(String host, int port) {
+    this.host = host;
+    this.port = port;
   }
-  
+
   @Override
   public int insert(Member member) throws Exception {
-    out.writeUTF("/member/add");
-    out.writeObject(member);
-    out.flush();
-    
-    if (!in.readUTF().equals("ok"))
-      throw new Exception(in.readUTF());
-    
-    return 1;
+    try (Socket socket = new Socket(host, port);
+        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+        ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
+
+      out.writeUTF("/member/add");
+      out.writeObject(member);
+      out.flush();
+
+      if (!in.readUTF().equals("ok"))
+        throw new Exception(in.readUTF());
+
+      return 1;
+    }
   }
-  
+
   @SuppressWarnings("unchecked")
   @Override
   public List<Member> findAll() throws Exception {
-    out.writeUTF("/member/list");
-    out.flush();
-    
-    if (!in.readUTF().equals("ok"))
-      throw new Exception(in.readUTF());
-    
-    return (List<Member>)in.readObject();
+    try (Socket socket = new Socket(host, port);
+        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+        ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
+
+      out.writeUTF("/member/list");
+      out.flush();
+
+      if (!in.readUTF().equals("ok"))
+        throw new Exception(in.readUTF());
+
+      return (List<Member>)in.readObject();
+    }
   }
-  
+
   @Override
   public Member findBy(int no) throws Exception {
-    out.writeUTF("/member/detail");
-    out.writeInt(no);
-    out.flush();
-    
-    if (!in.readUTF().equals("ok"))
-      throw new Exception(in.readUTF());
-    
-    return (Member)in.readObject();
+    try (Socket socket = new Socket(host, port);
+        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+        ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
+
+      out.writeUTF("/member/detail");
+      out.writeInt(no);
+      out.flush();
+
+      if (!in.readUTF().equals("ok"))
+        throw new Exception(in.readUTF());
+
+      return (Member)in.readObject();
+    }
   }
-  
+
   @Override
   public int update(Member member) throws Exception {
-    out.writeUTF("/member/update");
-    out.writeObject(member);
-    out.flush();
-    
-    if (!in.readUTF().equals("ok"))
-      throw new Exception(in.readUTF());
-    
-    return 1;
+    try (Socket socket = new Socket(host, port);
+        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+        ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
+
+      out.writeUTF("/member/update");
+      out.writeObject(member);
+      out.flush();
+
+      if (!in.readUTF().equals("ok"))
+        throw new Exception(in.readUTF());
+
+      return 1;
+    }
   }
-  
+
   @Override
   public int delete(int no) throws Exception {
-    out.writeUTF("/member/delete");
-    out.writeInt(no);
-    out.flush();
-    
-    if (!in.readUTF().equals("ok"))
-      throw new Exception(in.readUTF());
-    
-    return 1;
+    try (Socket socket = new Socket(host, port);
+        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+        ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
+
+      out.writeUTF("/member/delete");
+      out.writeInt(no);
+      out.flush();
+
+      if (!in.readUTF().equals("ok"))
+        throw new Exception(in.readUTF());
+
+      return 1;
+    }
   }
-  
+
 }
 
 
