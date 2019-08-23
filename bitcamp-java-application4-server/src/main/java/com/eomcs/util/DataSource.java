037d8b187924a19bf2390c1f7fd9a31095822550
eomjinyoung <jinyoung.eom@gmail.com>
2019-08-23 16:30:34 +0900

    bitcamp-java-application4 C/S 41_1

--- bitcamp-java-application4-server/src/main/java/com/eomcs/util/DataSource.java
+++ bitcamp-java-application4-server/src/main/java/com/eomcs/util/DataSource.java
@@ -2,10 +2,10 @@ package com.eomcs.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.util.ArrayList;
 
 // DAO가 사용할 커넥션 객체를 생성해주는 역할
-public class ConnectionFactory {
+public class DataSource {
   String jdbcDriver;
   String jdbcUrl;
   String username;
@@ -15,7 +15,10 @@ public class ConnectionFactory {
   // 현재 스레드의 값을 넣고 꺼낼 수 있는 도구를 준비한다. 
   ThreadLocal<Connection> localConnection = new ThreadLocal<>();
   
-  public ConnectionFactory(
+  // 재활용할 커넥션을 담을 바구니를 준비한다.
+  ArrayList<Connection> conPool = new ArrayList<>();
+  
+  public DataSource(
       String jdbcDriver, String jdbcUrl, String username, String password) {
     
     this.jdbcDriver = jdbcDriver;
@@ -28,12 +31,20 @@ public class ConnectionFactory {
     // ThreadLocal 도구를 사용하여 현재 스레드에서 커넥션 객체를 꺼낸다.
     Connection con = localConnection.get();
     
-    if (con == null) { // 없다면 새로 만들어 현재 스레드에 보관한다.
-      Class.forName(jdbcDriver);
-      con = new TxConnection(DriverManager.getConnection(
-              jdbcUrl, username, password));
+    if (con == null) { // 없다면,
       
-      // 생성한 커넥션을 리턴하기 전에 ThreadLocal 도구를 사용하여 현재 스레드에 보관한다.
+      if (conPool.size() > 0) {// 먼저 커넥션풀에서 커넥션을 찾아보고 있으면 꺼낸다.
+        con = conPool.remove(0);
+        System.out.println("기존 커넥션 사용!");
+        
+      } else { // 커넥션풀에 없다면 새로 만든다.
+        Class.forName(jdbcDriver);
+        con = new TxConnection(DriverManager.getConnection(
+            jdbcUrl, username, password));
+        System.out.println("새 커넥션 만들어 사용!");
+      }
+      
+      // 준비한 커넥션을 리턴하기 전에 ThreadLocal 도구를 사용하여 현재 스레드에 보관한다.
       localConnection.set(con);
     }
     
@@ -44,8 +55,10 @@ public class ConnectionFactory {
   public void clearConnection() {
     Connection con = localConnection.get();
     if (con != null) {
-      try {((TxConnection)con).realClose();} catch (SQLException e) {}
-      localConnection.remove();
+      localConnection.remove(); 
+      // 스레드에서 제거한 후, 커넥션 객체를 다시 커넥션풀에 저장한다.
+      conPool.add(con);
+      System.out.println("커넥션풀에 다시 저장!");
     }
   }
 }
---
+++ bitcamp-java-application4-server/v41_1/src/main/java/com/eomcs/lms/handler/BoardAddCommand.java
@@ -0,0 +1,32 @@
+package com.eomcs.lms.handler;
+
+import java.io.BufferedReader;
+import java.io.PrintStream;
+import com.eomcs.lms.dao.BoardDao;
+import com.eomcs.lms.domain.Board;
+import com.eomcs.util.Input;
+
+public class BoardAddCommand implements Command {
+  
+  private BoardDao boardDao;
+  
+  public BoardAddCommand(BoardDao boardDao) {
+    this.boardDao = boardDao;
+  }
+
+  @Override
+  public void execute(BufferedReader in, PrintStream out) {
+    try {
+      Board board = new Board();
+      board.setContents(Input.getStringValue(in, out, "내용? "));
+
+      boardDao.insert(board);
+      out.println("저장하였습니다.");
+      
+    } catch (Exception e) {
+      out.println("데이터 저장에 실패했습니다!");
+      System.out.println(e.getMessage());
+    }
+  }
+
+}
