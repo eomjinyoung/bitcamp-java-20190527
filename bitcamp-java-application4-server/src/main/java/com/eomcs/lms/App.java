d2f96b0b305f84b41542a6a27bd1f3f94d9d95e4
eomjinyoung <jinyoung.eom@gmail.com>
2019-08-26 18:04:07 +0900

    bitcamp-java-application4 C/S 43_1

--- bitcamp-java-application4-server/src/main/java/com/eomcs/lms/App.java
+++ bitcamp-java-application4-server/src/main/java/com/eomcs/lms/App.java
@@ -97,7 +97,7 @@ public class App {
       
       // Command 객체가 사용할 데이터 처리 객체를 준비한다.
       BoardDao boardDao = new BoardDaoImpl(sqlSessionFactory);
-      MemberDao memberDao = new MemberDaoImpl(dataSource);
+      MemberDao memberDao = new MemberDaoImpl(sqlSessionFactory);
       LessonDao lessonDao = new LessonDaoImpl(dataSource);
       PhotoBoardDao photoBoardDao = new PhotoBoardDaoImpl(dataSource);
       PhotoFileDao photoFileDao = new PhotoFileDaoImpl(dataSource);
