9005ec51d0a34fc6f477edf24bdc55a737eba3de
eomjinyoung <jinyoung.eom@gmail.com>
2019-08-29 11:17:02 +0900

    bitcamp-java-application4 C/S 44_1

---
+++ bitcamp-java-application4-server/v44_1/src/main/java/com/eomcs/lms/dao/PhotoBoardDao.java
@@ -0,0 +1,22 @@
+package com.eomcs.lms.dao;
+
+import java.util.List;
+import com.eomcs.lms.domain.PhotoBoard;
+
+// DAO 사용 규칙을 정의한다.
+public interface PhotoBoardDao {
+  int insert(PhotoBoard photoBoard) throws Exception;
+  List<PhotoBoard> findAll() throws Exception;
+  PhotoBoard findBy(int no) throws Exception;
+  PhotoBoard findWithFilesBy(int no) throws Exception;
+  int update(PhotoBoard photoBoard) throws Exception;
+  int delete(int no) throws Exception;
+}
+
+
+
+
+
+
+
+
---
+++ bitcamp-java-application4-server/v44_1/src/main/java/com/eomcs/lms/handler/BoardListCommand.java
@@ -0,0 +1,33 @@
+package com.eomcs.lms.handler;
+
+import java.io.BufferedReader;
+import java.io.PrintStream;
+import java.util.List;
+import com.eomcs.lms.dao.BoardDao;
+import com.eomcs.lms.domain.Board;
+
+public class BoardListCommand implements Command {
+  
+  private BoardDao boardDao;
+  
+  public BoardListCommand(BoardDao boardDao) {
+    this.boardDao = boardDao;
+  }
+  
+  @Override
+  public void execute(BufferedReader in, PrintStream out) {
+    try {
+      List<Board> boards = boardDao.findAll();
+      for (Board board : boards) {
+        out.printf("%s, %s, %s, %s\n", 
+            board.getNo(), board.getContents(), 
+            board.getCreatedDate(), board.getViewCount());
+      }
+      
+    } catch (Exception e) {
+      out.println("데이터 목록 조회에 실패했습니다!");
+      e.printStackTrace();
+    }
+  }
+
+}
