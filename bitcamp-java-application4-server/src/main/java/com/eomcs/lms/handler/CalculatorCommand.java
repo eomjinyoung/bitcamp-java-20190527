56c85c945f521f59fc6d1f6f6d05b78ccaf6f8e5
eomjinyoung <jinyoung.eom@gmail.com>
2019-09-02 15:38:05 +0900

    bitcamp-java-application4 C/S 49_1

---
+++ bitcamp-java-application4-server/src/main/java/com/eomcs/lms/handler/CalculatorCommand.java
@@ -0,0 +1,21 @@
+package com.eomcs.lms.handler;
+
+public class CalculatorCommand {
+
+}
+
+// 실습 과제:
+// => 다음과 같이 실행하도록 위 클래스를 완성하라!
+//
+/*
+명령> /calc/plus
+값1? 100
+값2? 200
+100 + 200 = 300
+
+명령> /calc/minus
+값1? 100
+값2? 200
+100 - 200 = -100
+
+ */
