8b8fde3405ab1bb76ac84c276b1a9a6db0d0664b
eomjinyoung <jinyoung.eom@gmail.com>
2019-09-05 11:39:14 +0900

    bitcamp-java-application4 C/S 51_1

---
+++ bitcamp-java-application4-server/v51_1/src/main/java/com/eomcs/lms/DatabaseConfig.java
@@ -0,0 +1,44 @@
+package com.eomcs.lms;
+
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@PropertySource("classpath:com/eomcs/lms/conf/jdbc.properties")
+public class DatabaseConfig {
+  
+  @Value("${jdbc.driver}") String jdbcDriver;
+
+  @Value("${jdbc.url}") String jdbcUrl;
+  
+  @Value("${jdbc.username}") String jdbcUsername;
+  
+  @Value("${jdbc.password}") String jdbcPassword;
+  
+  @Bean
+  public DataSource dataSource() {
+    DriverManagerDataSource ds = new DriverManagerDataSource();
+    ds.setDriverClassName(this.jdbcDriver); 
+    ds.setUrl(this.jdbcUrl);
+    ds.setUsername(this.jdbcUsername);
+    ds.setPassword(this.jdbcPassword);
+    return ds;
+  }
+  
+  @Bean
+  public PlatformTransactionManager transactionManager(DataSource dataSource) {
+    return new DataSourceTransactionManager(dataSource);
+  }
+}
+
+
+
+
+
+
