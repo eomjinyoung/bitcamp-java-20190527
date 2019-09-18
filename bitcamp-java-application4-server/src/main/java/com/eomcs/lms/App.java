565ff1dea3cebfa07bf1c235889546fed51584e8
eomjinyoung <jinyoung.eom@gmail.com>
2019-09-18 10:43:16 +0900

    bitcamp-java-application4 56_2

--- bitcamp-java-application4-server/src/main/java/com/eomcs/lms/App.java
+++ bitcamp-java-application4-server/src/main/java/com/eomcs/lms/App.java
@@ -1,5 +1,11 @@
-// v56_1 : 서블릿을 이용하여 클라이언트 요청 처리하기
+// v56_2 : 리스너를 이용하여 스프링 IoC 컨테이너 준비하기
 package com.eomcs.lms;
+
+// 작업:
+// => ContextLoaderListener 리스너 생성
+// => AppInitServlet의 역할을 ContextLoaderListener로 이전
+// => web.xml 변경
+// => AppInitServlet 삭제
 
 // dummy 클래스!
 // => 기존 버전에서 계속 존재했던 클래스라서 그대로 둠.
--- bitcamp-java-application4-server/src/main/java/com/eomcs/lms/listener/ContextLoaderListener.java
+++ bitcamp-java-application4-server/src/main/java/com/eomcs/lms/listener/ContextLoaderListener.java
@@ -1,35 +1,36 @@
-package com.eomcs.lms;
+package com.eomcs.lms.listener;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.lang.reflect.Constructor;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import com.eomcs.util.RequestMappingHandlerMapping;
 
 // 역할:
-// => 다른 서블릿이 사용할 객체를 준비한다.
-// => Spring IoC 컨테이너를 준비하여 ServletContext 보관소에 저장한다.
-// => 이 서블릿은 클라이언트가 직접 실행하는 서블릿이 아니기 때문에 
-//    서버에 배치할 때 URL을 지정하지 않는다.
-//    또한 service() 메서드를 오버라이딩 하지 않는다.
+// => 웹 애플리케이션이 시작될 때 보고를 받는다.
+// => Spring IoC 컨테이너를 준비한다.
+// => ServletContext 보관소에 IoC 컨테이너를 저장한다.
 //
-public class AppInitServlet extends HttpServlet {
-  private static final long serialVersionUID = 1L;
-
+public class ContextLoaderListener implements ServletContextListener {
+  
   // Log4j의 로그 출력 도구를 준비한다.
-  private static final Logger logger = LogManager.getLogger(AppInitServlet.class);
-
-  ApplicationContext appCtx;
-  RequestMappingHandlerMapping handlerMapping;
-  int state;
+  private static final Logger logger = 
+      LogManager.getLogger(ContextLoaderListener.class);
 
   @Override
-  public void init() throws ServletException {
+  public void contextInitialized(ServletContextEvent sce) {
+    // 웹 애플리케이션이 시작될 때 서블릿 컨테이너가 호출하는 메서드
+    
+    ServletContext sc = sce.getServletContext();
+    ApplicationContext appCtx = null;
+
     // Spring IoC 컨테이너의 클래스 이름을 가져오기
-    String contextClass = getInitParameter("contextClass");
+    String contextClass = sc.getInitParameter("contextClass");
     if (contextClass != null) {
       try {
         // web.xml 에 설정한 IoC 컨테이너의 클래스를 로딩한다.
@@ -39,14 +40,17 @@ public class AppInitServlet extends HttpServlet {
         Constructor<?> constuctor = clazz.getConstructor(String[].class);
         
         // web.xml에서 IoC 컨테이너가 사용할 패키지 이름을 가져온다.
-        String basePackageName = getInitParameter("contextConfigLocation");
+        String basePackageName = sc.getInitParameter("contextConfigLocation");
         
         // 생성자를 이용하여 IoC 컨테이너 객체를 만든다.
         appCtx = (ApplicationContext) constuctor.newInstance(
             (Object) new String[] {basePackageName});
         
       } catch (Exception e) {
-        throw new ServletException(e);
+        StringWriter out = new StringWriter();
+        e.printStackTrace(new PrintWriter(out));
+        
+        logger.error(out.toString());
       }
     } else {
       // 파라미터로 전달한 패키지를 뒤져서
@@ -58,7 +62,7 @@ public class AppInitServlet extends HttpServlet {
     }
     
     // Spring IoC 컨테이너를 서블릿에서 사용할 수 있도록 ServletContext 보관소에 저장한다.
-    getServletContext().setAttribute("iocContainer", appCtx);
+    sc.setAttribute("iocContainer", appCtx);
     
     String[] beanNames = appCtx.getBeanDefinitionNames();
     logger.debug("[Spring IoC 컨테이너 객체들]------------");
@@ -70,6 +74,7 @@ public class AppInitServlet extends HttpServlet {
     logger.debug("------------------------------------");
   }
 }
+
 
 
 
