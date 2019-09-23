9cfe335fe91fb94e86fde2a5f9bf2f25bedaf02d
eomjinyoung <jinyoung.eom@gmail.com>
2019-09-23 17:38:10 +0900

    bitcamp-java-application4 58_2

--- bitcamp-java-application4-server/src/main/java/com/eomcs/lms/App.java
+++ bitcamp-java-application4-server/src/main/java/com/eomcs/lms/App.java
@@ -1,9 +1,11 @@
-// v58_1 : Front Controller 도입
+// v58_2 : Page Controller를 일반 클래스(POJO; Plain Old Java Object)로 전환하기
 package com.eomcs.lms;
 
 // 작업:
-// => DispatcherServlet 생성
-// => 다른 서블릿 변경
+// => Command 인터페이스 생성
+// => XxxServlet 을 XxxCommand 로 전환
+// => 커맨드 객체의 관리 주체를 서블릿 컨테이너에서 Spring IoC 컨테이너로 이전.
+// => DispatcherServlet 변경
 //
 //
 // dummy 클래스!
--- bitcamp-java-application4-server/src/main/java/com/eomcs/lms/servlet/DispatcherServlet.java
+++ bitcamp-java-application4-server/src/main/java/com/eomcs/lms/servlet/DispatcherServlet.java
@@ -1,7 +1,6 @@
 package com.eomcs.lms.servlet;
 
 import java.io.IOException;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +9,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import com.eomcs.lms.handler.Command;
 
 @MultipartConfig(maxFileSize = 1024 * 1024 * 10)
 @WebServlet("/app/*")
@@ -17,6 +18,14 @@ public class DispatcherServlet extends HttpServlet {
   private static final long serialVersionUID = 1L;
   
   private static final Logger log = LogManager.getLogger(DispatcherServlet.class);
+  
+  private ApplicationContext iocContainer;
+  
+  @Override
+  public void init() throws ServletException {
+    iocContainer = 
+        (ApplicationContext) getServletContext().getAttribute("iocContainer");
+  }
   
   @Override
   public void service(HttpServletRequest request, HttpServletResponse response) 
@@ -27,34 +36,34 @@ public class DispatcherServlet extends HttpServlet {
     log.debug(String.format("요청 => servletPath: %s, pathInfo: %s", 
         servletPath, pathInfo));
     
-    // 클라이언트가 요청한 서블릿(page controller)에게 실행을 위임한다.
-    RequestDispatcher rd = request.getRequestDispatcher(pathInfo);
-    rd.include(request, response);
-    
-    Exception error = (Exception) request.getAttribute("error");
-    if (error != null) {
-      request.getRequestDispatcher("/jsp/error.jsp").forward(request, response);
-      return;
-    }
-    
-    // 페이지 컨트롤러 작업을 수행한 후 JSP를 실행한다.
-    String contentType = (String) request.getAttribute("contentType");
-    if (contentType != null) {
-      response.setContentType(contentType);
-    } else {
-      response.setContentType("text/html;charset=UTF-8");
-    }
-    
-    String viewUrl = (String) request.getAttribute("viewUrl");
-    if (viewUrl != null) {
-      if (viewUrl.startsWith("redirect:")) {
-        response.sendRedirect(viewUrl.substring(9)); // "redirect:list"
+    try {
+      // 클라이언트 요청을 처리할 Command를 구현한 페이지 컨트롤러를 찾는다.
+      Command pageController = (Command) iocContainer.getBean(pathInfo);
+      
+      // 페이지 컨트롤러에게 실행을 위임한다.
+      String viewUrl = pageController.execute(request, response);
+      
+      // 응답 콘텐트의 MIME 타입과 문자집합을 설정한다.
+      String contentType = (String) request.getAttribute("contentType");
+      if (contentType != null) {
+        response.setContentType(contentType);
       } else {
-        rd = request.getRequestDispatcher(viewUrl);
-        rd.include(request, response);
+        response.setContentType("text/html;charset=UTF-8");
       }
+      
+      // 페이지 컨트롤러 작업을 수행한 후 리턴 URL에 따라 JSP를 실행한다.
+      if (viewUrl != null) {
+        if (viewUrl.startsWith("redirect:")) {
+          response.sendRedirect(viewUrl.substring(9)); // "redirect:list"
+        } else {
+          request.getRequestDispatcher(viewUrl).include(request, response);
+        }
+      }
+      
+    } catch (Exception e) {
+      request.setAttribute("error", e);
+      request.getRequestDispatcher("/jsp/error.jsp").forward(request, response);
     }
-    
   }
 }
 
