a54d9007eea99086a940659aa0b54e67390ba028
eomjinyoung <jinyoung.eom@gmail.com>
2019-09-30 11:59:19 +0900

    bitcamp-java-spring-webmvc

--- bitcamp-java-spring-webmvc/src.12/main/java/bitcamp/app2/Controller05_2.java
+++ bitcamp-java-spring-webmvc/src.12/main/java/bitcamp/app2/Controller05_2.java
@@ -1,22 +1,24 @@
-// JSON 콘텐트 출력하기
+// JSON 콘텐트 출력하기 - @RestController
 package bitcamp.app2;
 
 import java.sql.Date;
 import java.util.ArrayList;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import java.util.HashMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import com.google.gson.Gson;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller 
-@RequestMapping("/c05_1")
-public class Controller05_1 {
+@RestController
+// => 페이지 컨트롤러를 @RestController로 선언하면,
+//    리턴 값은 HttpMessageConverter에 의해 자동으로 변환된다.
+// => @ResponseBody를 붙일 필요가 없다.
+//
+@RequestMapping("/c05_2")
+public class Controller05_2 {
 
   ArrayList<Board> list = new ArrayList<>();
   
-  public Controller05_1() {
+  public Controller05_2() {
     list.add(new Board(1, "제목입니다1", "내용", "홍길동", 10, Date.valueOf("2019-5-1")));
     list.add(new Board(2, "제목입니다2", "내용", "홍길동2", 11, Date.valueOf("2019-5-2")));
     list.add(new Board(3, "제목입니다3", "내용", "홍길동3", 12, Date.valueOf("2019-5-3")));
@@ -32,45 +34,40 @@ public class Controller05_1 {
     list.add(new Board(13, "제목입니다13", "내용", "홍길동13", 13, Date.valueOf("2019-10-1")));
   }
   
-  // 1) JSP에서 JSON 형식의 콘텐트 출력하기
   // 테스트:
-  //   http://.../app2/c05_1/h1
+  //   http://.../app2/c05_2/h1
   @GetMapping("h1")
-  public void handler1(Model model) {
-    model.addAttribute("list", this.list);
-  }
-  
-  // 2) Google Gson 라이브러리를 사용하여 JSON 형식의 콘텐트 출력하기
-  // => mvnrepository.com에서 gson 검색하여 라이브러리 정보를 가져온다.
-  // => build.gradle 파일에 추가한다.
-  // => '$gradle eclipse' 실행한다.
-  // => 이클립스에서 프로젝트를 리프래시 한다.
-  // 테스트:
-  //   http://.../app2/c05_1/h2
-  @GetMapping(value="h2", produces="text/plain;charset=UTF-8")
-  @ResponseBody
-  public String handler2() {
-    return new Gson().toJson(this.list);
-  }
-  
-  
-  // 3) Google Gson 라이브러리를 사용하여 JSON 형식의 콘텐트 출력하기 II
-  // => 페이지 컨트롤러의 리턴 값이 String이 아니면 
-  //    프론트 컨트롤러는 
-  //    Google의 Gson 라이브러리나 Jackson 라이브러리를 사용하여 
-  //    자동으로 JSON 형식의 문자열로 만들어 클라이언트로 출력한다.
-  // => 단 Gson 또는 Jackson 라이브러리가 있어야 한다.
-  //    둘 다 있다면 Jackson 라이브러리가 기본으로 사용된다.
-  // => build.gradle 파일에서 gson 또는 jackson 라이브러리를 추가하는 부분의 
-  //    주석을 참고하라!
-  // 테스트:
-  //   http://.../app2/c05_1/h3
-  @GetMapping("h3")
-  @ResponseBody
-  public Object handler3() {
+  public Object handler1() {
     return this.list; // JSON 형식의 문자열은 자동으로 UTF-8로 인코딩 된다.
   }
 
+  // 테스트:
+  //   http://.../app2/c05_2/h2
+  @GetMapping(value="h2", produces="text/plain;charset=UTF-8")
+  public String handler2() {
+    return "안녕하세요!"; // String 타입은 그대로 출력한다. 
+                       // 단 출력 문자열의 인코딩을 지정해야 한글이 깨지지 않는다.
+  }
+  
+  // 테스트:
+  //   http://.../app2/c05_2/h3
+  @GetMapping("h3")
+  public int handler3() {
+    return 100; // primitive 타입의 값도 그대로 출력한다.
+  }
+  
+  // 테스트:
+  //   http://.../app2/c05_2/h4
+  @GetMapping("h4")
+  public Object handler4() {
+    // primitive 타입이나 String 타입의 값을 JSON 형식으로 출력하려면 
+    // 도메인 객체나 맵 객체 담아 리턴하라!
+    HashMap<String,Object> content = new HashMap<>();
+    content.put("v1", 100);
+    content.put("v2", "Hello");
+    
+    return content;
+  }
 }
 
 
---
+++ bitcamp-java-spring-webmvc/src.12/main/java/bitcamp/config/App1Config.java
@@ -0,0 +1,26 @@
+package bitcamp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+@ComponentScan("bitcamp.app1")
+public class App1Config {
+  
+  @Bean
+  public MultipartResolver multipartResolver() {
+    // 스프링 방식으로 파일 업로드를 처리하고 싶다면,
+    // 이 메서드를 통해 MultipartResolver 구현체를 생성해야 한다.
+    CommonsMultipartResolver mr = new CommonsMultipartResolver();
+    mr.setMaxUploadSize(10000000);
+    mr.setMaxInMemorySize(2000000);
+    mr.setMaxUploadSizePerFile(5000000);
+    return mr; 
+  }
+}
+
+
+
+
+
