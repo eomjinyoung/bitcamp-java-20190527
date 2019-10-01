2ff7944092d2e21eda87d13c644e3b16de237083
eomjinyoung <jinyoung.eom@gmail.com>
2019-10-01 12:30:59 +0900

    bitcamp-java-application4 60_1

--- bitcamp-java-application4-server/src/main/java/com/eomcs/lms/web/MemberController.java
+++ bitcamp-java-application4-server/src/main/java/com/eomcs/lms/web/MemberController.java
@@ -4,98 +4,76 @@ import java.io.File;
 import java.util.List;
 import java.util.UUID;
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.ServletContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
-import com.eomcs.lms.dao.MemberDao;
 import com.eomcs.lms.domain.Member;
+import com.eomcs.lms.service.MemberService;
 
 @Controller
 @RequestMapping("/member")
 public class MemberController {
 
-  @Resource 
-  private MemberDao memberDao;
+  @Resource private MemberService memberService;
 
+  String uploadDir;
+
+  public MemberController(ServletContext sc) {
+    uploadDir = sc.getRealPath("/upload/member");
+  }
+  
   @GetMapping("form")
   public void form() {
   }
   
   @PostMapping("add")
-  public String add(
-      HttpServletRequest request, 
-      Member member, 
-      MultipartFile file) throws Exception {
-    
-    String uploadDir = request.getServletContext().getRealPath("/upload/member");
-    
-    // 업로드 된 사진 파일 처리
-    if (!file.isEmpty()) {
-      String filename = UUID.randomUUID().toString();
-      member.setPhoto(filename);
-      file.transferTo(new File(uploadDir + "/" + filename));
-    }
-
-    memberDao.insert(member);
+  public String add(Member member, MultipartFile file) throws Exception {
+    member.setPhoto(writeFile(file));
+    memberService.insert(member);
     return "redirect:list";
   }
   
   @GetMapping("delete")
-  public String delete(int no) 
-      throws Exception {
-
-    if (memberDao.delete(no) == 0) {
-      throw new Exception("해당 데이터가 없습니다.");
-    }
+  public String delete(int no) throws Exception {
+    memberService.delete(no);
     return "redirect:list";
   }
   
   @GetMapping("detail")
-  public void detail(Model model, int no) 
-      throws Exception {
-
-    Member member = memberDao.findBy(no);
-    if (member == null) {
-      throw new Exception("해당 번호의 데이터가 없습니다!");
-    } 
-
+  public void detail(Model model, int no) throws Exception {
+    Member member = memberService.get(no);
     model.addAttribute("member", member);
   }
   
   @GetMapping("list")
   public void list(Model model) throws Exception {
-
-    List<Member> members = memberDao.findAll();
+    List<Member> members = memberService.list();
     model.addAttribute("members", members);
   }
   
   @GetMapping("search")
   public void search(Model model, String keyword) throws Exception {
-
-    List<Member> members = memberDao.findByKeyword(keyword);
+    List<Member> members = memberService.search(keyword);
     model.addAttribute("members", members);
   }
   
   @PostMapping("update")
-  public String update(
-      HttpServletRequest request,
-      Member member,
-      MultipartFile file) 
-      throws Exception {
-    
-    String uploadDir = request.getServletContext().getRealPath("/upload/member");
-
-    // 업로드 된 사진 파일 처리
-    if (!file.isEmpty()) {
-      String filename = UUID.randomUUID().toString();
-      member.setPhoto(filename);
-      file.transferTo(new File(uploadDir + "/" + filename));
-    }
-    memberDao.update(member);
+  public String update(Member member, MultipartFile file) throws Exception {
+    member.setPhoto(writeFile(file));
+    memberService.update(member);
     return "redirect:list";
+  }
+  
+  private String writeFile(MultipartFile file) throws Exception {
+    if (file.isEmpty())
+      return null;
+    
+    String filename = UUID.randomUUID().toString();
+    file.transferTo(new File(uploadDir + "/" + filename));
+    return filename;
   }
 }
