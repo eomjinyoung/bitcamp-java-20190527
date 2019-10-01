441c18fb87d5d024a519422abf12fb18bdc27b15
eomjinyoung <jinyoung.eom@gmail.com>
2019-10-01 12:17:29 +0900

    bitcamp-java-application4 60_1

---
+++ bitcamp-java-application4-server/src/main/java/com/eomcs/lms/service/impl/DefaultMemberService.java
@@ -0,0 +1,53 @@
+package com.eomcs.lms.service.impl;
+
+import java.util.List;
+import javax.annotation.Resource;
+import org.springframework.stereotype.Service;
+import com.eomcs.lms.dao.MemberDao;
+import com.eomcs.lms.domain.Member;
+import com.eomcs.lms.service.MemberService;
+
+// MemberService 기본 구현체 
+//
+@Service
+public class DefaultMemberService implements MemberService {
+
+  @Resource
+  private MemberDao memberDao;
+
+  @Override
+  public void insert(Member member) throws Exception {
+    memberDao.insert(member);
+  }
+
+  @Override
+  public void delete(int no) throws Exception {
+    if (memberDao.delete(no) == 0) {
+      throw new Exception("해당 데이터가 없습니다.");
+    }
+  }
+
+  @Override
+  public Member get(int no) throws Exception {
+    Member member = memberDao.findBy(no);
+    if (member == null) {
+      throw new Exception("해당 번호의 데이터가 없습니다!");
+    } 
+    return member;
+  }
+
+  @Override
+  public List<Member> list() throws Exception {
+    return memberDao.findAll();
+  }
+  
+  @Override
+  public List<Member> search(String keyword) throws Exception {
+    return memberDao.findByKeyword(keyword);
+  }
+
+  @Override
+  public void update(Member member) throws Exception {
+    memberDao.update(member);
+  }
+}
