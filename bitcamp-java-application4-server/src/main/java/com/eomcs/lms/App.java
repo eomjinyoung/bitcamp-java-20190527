cc8951b83dcedcb153bea152683a0337e779fef1
eomjinyoung <jinyoung.eom@gmail.com>
2019-10-14 12:59:50 +0900

    bitcamp-java-application4 61_1

--- bitcamp-java-application4-server/src/main/java/com/eomcs/lms/App.java
+++ bitcamp-java-application4-server/src/main/java/com/eomcs/lms/App.java
@@ -1,22 +1,9 @@
-// v60_1 : Service 컴포넌트 도입하기
+// v61_1 : Back-end 와 Front-end 분리하기
 package com.eomcs.lms;
 
 // 작업:
-// => com.eomcs.lms.service 패키지 생성
-// => BoardService, MemberService, LessonService, PhotoBoardService 인터페이스 생성
-// => com.eomcs.lms.service.impl 패키지 생성
-// => DefaultBoardService, ... 클래스 생성
+// => /webapp/html/board/list.html 생성
 //
-// => XxxxController 변경
-//    - DAO를 직접 사용하는 대신에 Service 컴포넌트를 사용하도록 변경한다.
-// => PhotoFileWriter 생성
-//    - 파일 업로드 처리 코드 분리
-//
-// => com.eomcs.lms.web.json 패키지 생성
-// => web의 XxxxController를 web.json으로 복사
-// => XxxxController의 path 변경
-// => @RestController로 변경
-// => 리턴 값을 JSON으로 변경
 //
 // dummy 클래스!
 // => 기존 버전에서 계속 존재했던 클래스라서 그대로 둠.
