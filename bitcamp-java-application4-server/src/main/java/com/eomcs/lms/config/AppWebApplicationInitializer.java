6c16dffa83843e81bd622cc24d9c0b4cf2226d1b
eomjinyoung <jinyoung.eom@gmail.com>
2019-10-18 09:09:15 +0900

    CharacterEncodingFilter를 스프링 클래스로 교체

--- bitcamp-java-application4-server/src/main/java/com/eomcs/lms/config/AppWebApplicationInitializer.java
+++ bitcamp-java-application4-server/src/main/java/com/eomcs/lms/config/AppWebApplicationInitializer.java
@@ -1,5 +1,7 @@
 package com.eomcs.lms.config;
 
+import javax.servlet.Filter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class AppWebApplicationInitializer 
@@ -24,6 +26,13 @@ public class AppWebApplicationInitializer
   @Override
   protected String getServletName() {
     return "app";
+  }
+  
+  @Override
+  protected Filter[] getServletFilters() {
+    return new Filter[] {
+        new CharacterEncodingFilter("UTF-8")
+        /*, new AuthFilter()*/ };
   }
 }
 
